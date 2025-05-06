4b349ce8c4def3be73210abe44b6e5bcdbfaaaff
Artur Siqueira <artur.siqueira.98@gmail.com>
2025-05-05 21:53:34 -0300

    Restante das atividades da aula 2

---
+++ aula02-exemplos/atividade/validador_dia_util.py
@@ -0,0 +1,8 @@
+# Quero saber se um dia é útil ou não
+
+dia = input("Digite o dia da semana: ").lower()
+
+if dia =="sábado" or dia == "domingo":
+    print("Não é um dia útil")
+else:
+    print("É um dia útil")
---
+++ aula02-exemplos/atividade/validar_intervalo_numero.py
@@ -0,0 +1,8 @@
+# Quero saber se um número está entre 10 e 20
+
+numero = int(input("Digite um número: "))
+
+if numero > 10 and numero < 20:
+    print("O número está entre 10 e 20")
+else:  
+    print("O número não está entre 10 e 20")
